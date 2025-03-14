8997760317f6cb5dabec6ed98bef476f0c3d703f
ilshat.nizamov <ilshatn96@gmail.com>
2025-03-14 15:00:17 +0200

    add clients

---
+++ src/main/java/com/assignment/clients/AuthenticationClient.java
@@ -0,0 +1,33 @@
+package com.assignment.clients;
+
+import com.assignment.models.reqs.LoginRequest;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static io.restassured.RestAssured.given;
+
+public class AuthenticationClient extends BaseClient {
+
+    private static final Logger logger = LoggerFactory.getLogger(AuthenticationClient.class);
+
+    public AuthenticationClient(String baseUrl) {
+        super(baseUrl);
+    }
+
+    public Response login(LoginRequest requestModel) {
+        return given()
+                .contentType(ContentType.JSON)
+                .body(requestModel)
+                //can be added to config
+                .header("brandid",30)
+                .header("channelid", 1)
+                .when()
+                .post(getBaseUrl() + "LoginUser")
+                .then()
+                .statusCode(200)
+                .extract()
+                .response();
+    }
+}
