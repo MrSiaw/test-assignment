04ea6483a10e06a5efd2d560e5467ac51c41c9f4
ilshat.nizamov <ilshatn96@gmail.com>
2025-03-14 14:58:46 +0200

    init

---
+++ build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    id("java")
+    id("io.qameta.allure") version "2.11.2"
+}
+
+group = "com.assignment"
+version = "1.0-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.testng:testng:7.7.1")
+    implementation("com.codeborne:selenide:6.19.1")
+    implementation("org.slf4j:slf4j-simple:2.0.9")
+    implementation("io.qameta.allure:allure-testng:2.11.2")
+    implementation("io.rest-assured:rest-assured:5.4.0")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.17.3")
+
+
+}
+
+tasks.test {
+    useTestNG {
+        suites("src/test/resources/testng.xml")
+        listeners.add("io.qameta.allure.testng.AllureTestNg")
+    }
+}
