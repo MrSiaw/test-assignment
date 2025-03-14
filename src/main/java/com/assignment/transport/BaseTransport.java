c10e3d71bb8ced6fed9c4e48395a36a7abc38d6c
ilshat.nizamov <ilshatn96@gmail.com>
2025-03-14 15:02:08 +0200

    add transport

---
+++ src/main/java/com/assignment/transport/BaseTransport.java
@@ -0,0 +1,30 @@
+package com.assignment.transport;
+
+import com.assignment.utils.JacksonMapper;
+import io.restassured.response.Response;
+
+import static com.assignment.config.Config.BASE_URL_API;
+
+public abstract class BaseTransport {
+
+    /**
+     * Return service url
+     * @param serviceName   -   service name
+     * @return  -   url
+     */
+    protected static String getServiceUrl(String serviceName) {
+        return BASE_URL_API + serviceName + "/";
+    }
+
+    /**
+     * Conver response body to object
+     * @param response  -   response
+     * @param clazz -   model class
+     * @return  -   instance of a model class
+     * @param <T>
+     */
+    public static <T> T jsonToObject(Response response, Class<T> clazz) {
+        return JacksonMapper.jsonToObject(response.asPrettyString(), clazz);
+    }
+
+}
