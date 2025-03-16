da02bfb07d9d9f2ebf3f6716fe665e96cba758f1
ilshat.nizamov <ilshatn96@gmail.com>
2025-03-16 11:55:51 +0200

    add Equivalence for the form

--- src/test/java/test/web/login/LoginFormTest.java
+++ src/test/java/test/web/login/LoginFormTest.java
@@ -18,8 +18,29 @@ public class LoginFormTest extends BaseWebTest {
     private static final String VALID_USER_PHONE = "962323422";
     private static final String INVALID_USER_PHONE = "962323422";
     private static final String VALID_USER_PASS = "valid_pass";
+    private static final String ERROR_PHONE_OR_PASS_INVALID = "The Phone number or password entered is invalid";
+    private static final String ERROR_EMPTY_PHONE = "Phone number is required";
+    private static final String ERROR_EMPTY_PASS = "Password is required";
 
-    //region some tech
+    /**
+     * Open login page from upcoming events, fill phone and pass and press submit
+     * Check that error with message appears
+     *
+     * @param phone                -   phone number
+     * @param pass                 -   pass
+     * @param expectedErrorMessage -    expected error message
+     */
+    private void failedLoginWithError(String phone, String pass, String expectedErrorMessage) {
+        UpcomingEventPage upcomingEventPage = new UpcomingEventPage();
+        upcomingEventPage.checkIsLoggedIn(false);
+        upcomingEventPage
+                .openLoginPage()
+                .login(phone, pass)
+                .checkErrorMessageText(expectedErrorMessage);
+        upcomingEventPage.checkIsLoggedIn(false);
+    }
+
+    //Equivalence
     @Test(testName = "Successful login")
     //@AllureId("") should be some id
     public void successfulLogin() {
@@ -28,85 +49,34 @@ public class LoginFormTest extends BaseWebTest {
         upcomingEventPage
                 .openLoginPage()
                 .login(VALID_USER_PHONE, VALID_USER_PASS);
-        upcomingEventPage.checkIsLoggedIn(false);
+        upcomingEventPage.checkIsLoggedIn(true);
     }
 
-    @Test(testName = "неверный пароль")
+    //Equivalence
+    @Test(testName = "Invalid Password")
     public void invalidPassword() {
-        new UpcomingEventPage()
-                .openLoginPage()
-                .login(VALID_USER_PHONE, "213")
-                .checkErrorMessageText("The Phone number or password entered is invalid");
+        failedLoginWithError(VALID_USER_PHONE, VALID_USER_PASS, ERROR_PHONE_OR_PASS_INVALID);
     }
 
-    //endregion
-
-    @Test(testName = "пустые поля")
-    public void testEmptyFields() {
-        new UpcomingEventPage()
-                .openLoginPage()
-                .login("", "")
-                .checkErrorMessageText("");
+    //Equivalence
+    @Test(testName = "Invalid user phone")
+    public void invalidUserPhone() {
+        failedLoginWithError(INVALID_USER_PHONE, VALID_USER_PASS, ERROR_PHONE_OR_PASS_INVALID);
     }
 
-    @Test(testName = "")
-    public void testInvalidPhoneFormat() {
-        new UpcomingEventPage()
-                .openLoginPage()
-                .login(INVALID_USER_PHONE, VALID_USER_PASS)
-                .checkErrorMessageText("The Phone number or password entered is invalid");
+    //Equivalence
+    @Test(testName = "Empty phone number")
+    public void emptyPhoneNumber() {
+        failedLoginWithError("", VALID_USER_PASS, ERROR_EMPTY_PHONE);
     }
 
-    @Test(testName = "")
-    public void testEmptyPassword() {
-        new UpcomingEventPage()
-                .openLoginPage()
-                .login(INVALID_USER_PHONE, "")
-                .checkErrorMessageText("");
+    //Equivalence
+    @Test(testName = "Empty password")
+    public void emptyPassword() {
+        failedLoginWithError(VALID_USER_PHONE, "", ERROR_EMPTY_PASS);
     }
 
-    @Test(testName = "")
-    public void testShortPhoneNumber() {
-        new UpcomingEventPage()
-                .openLoginPage()
-                .login("12", VALID_USER_PASS)
-                .checkErrorMessageText("");
-    }
-
-    @Test(testName = "")
-    public void testLongPhoneNumber() {
-        new UpcomingEventPage()
-                .openLoginPage()
-                .login("1231245124214312", VALID_USER_PASS)
-                .checkErrorMessageText("");
-    }
-
-    @Test(testName = "")
-    public void testShortPassword() {
-        LoginPage loginPage = new UpcomingEventPage().openLoginPage().login("+1234567890", "123");
-        //assertTrue(loginPage.isErrorMessageDisplayed());
-    }
-
-    @Test(testName = "")
-    public void testLongPassword() {
-        //LoginPage loginPage =
-        //assertTrue(loginPage.isErrorMessageDisplayed());
-    }
-
-    @Test
-    public void phoneFieldValidation(String phone, String expected) {
-        new UpcomingEventPage()
-                .openLoginPage()
-                .login("+1234567890", "aVeryLongPasswordExceedingLimits");
-    }
-
-    @Test
-    public void passwordFieldValidation(String password, String expected) {
-        new UpcomingEventPage()
-                .openLoginPage()
-                .login("+1234567890", "aVeryLongPasswordExceedingLimits");
-    }
-
+    //Could be checks for login attempts, but couldn't find valid phone to find
 
     //Phone field validation
     //Boundary Value
