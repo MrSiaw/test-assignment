54c97bdbb12b7348f84b93d36fd02a48d9c6382a
ilshat.nizamov <ilshatn96@gmail.com>
2025-03-16 10:27:26 +0200

    add "filter upcoming events with odds >= 1.5 and <=3.34"

--- src/test/java/test/web/events/UpcomingEventListTest.java
+++ src/test/java/test/web/events/UpcomingEventListTest.java
@@ -18,8 +18,8 @@ public class UpcomingEventListTest extends BaseWebTest {
 
     private static final Logger logger = LoggerFactory.getLogger(UpcomingEventListTest.class);
 
-    @Test(testName = "")
-    public void upcomingEventsList() {
+    @Test(testName = "filter upcoming events with odds >= 1.5 and <=3.34")
+    public void upcomingEventsFilter() {
         UpcomingEventPage upcomingEventPage = new UpcomingEventPage();
         upcomingEventPage.open();
         HashMap <String, List<String>> str = upcomingEventPage
@@ -29,13 +29,25 @@ public class UpcomingEventListTest extends BaseWebTest {
         logger.info("before");
         str.forEach((key, value) -> logger.info("{} - {}", key, value));
         logger.info("after");
+        //this could be a separate method, some kind of a OddsUtil
+        //it depends on the following tests, where it is better to apply this method.
         str
                 .entrySet()
                 .stream()
                 .filter((entry) -> entry
                         .getValue()
                         .stream()
-                        .allMatch(odd -> Double.parseDouble(odd) >= 1.50d && Double.parseDouble(odd) <= 3.34d))
+                        .allMatch(odd -> {
+                            try {
+                                return Double.parseDouble(odd) >= 1.50d && Double.parseDouble(odd) <= 3.34d;
+                            } catch (NumberFormatException exception) {
+                                throw new AssertionError("Can't convert given odd to double: " + odd);
+                            }
+                        }))
                 .forEach(entry -> logger.info("{} - {}", entry.getKey(), entry.getValue()));
+        //Now we have a list of events id's that matches given filter 1.5 >= x <= 3.34. We can use this id for REST API of SQL
+        //For example, we can add a filter to search only for those rows where all 8 coefficients are available
+        //Either the number of coefficients should be 5 >= odds_count <= 8
+        //I don't get which assert we can add here, cause I don't know where I can get an expected
     }
 }
