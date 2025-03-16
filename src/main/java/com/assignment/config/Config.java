43f5f78d1ffe8769d6a6689865671713ba9c8e16
ilshat.nizamov <ilshatn96@gmail.com>
2025-03-16 10:23:58 +0200

    add todo, fix Config

--- src/main/java/com/assignment/config/Config.java
+++ src/main/java/com/assignment/config/Config.java
@@ -1,9 +1,9 @@
 package com.assignment.config;
 
-//should be form props file
+//TODO should be form props file
 public class Config {
     public static final String BASE_URL = "https://s.gsb.co.zm/";
-    public static final String BASE_URL_API = "https://s.gsb.co.zm/services/clapi/api/";
+    public static final String BASE_URL_API = BASE_URL + "services/clapi/api/";
     public static final String DEFAULT_PHONE_CODE = "+260";
 
 }
